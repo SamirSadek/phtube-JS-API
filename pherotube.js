4d95b663c92d7d20f9bcb328cb25f42312ac61f7
Samir Sadek <samirsadek2009@gmail.com>
2023-09-02 00:42:16 +0600

    done assignment without sorting

--- pherotube.js
+++ pherotube.js
@@ -1,48 +1,53 @@
-const handleCategory =async() =>{
+const handleCategory = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await response.json();
+  const category = data.data;
 
-    const response = await fetch("https://openapi.programming-hero.com/api/videos/categories");
-    const data =await response.json();
-    const category = data.data;
-    
-
-    const menuContainer = document.getElementById('menu-container');
-    category.forEach((category) => {
-        const div = document.createElement('div');
-        div.innerHTML = `
+  const menuContainer = document.getElementById("menu-container");
+  category.forEach((category) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
         <button onclick=loadCardContainer('${category.category_id}'); class="btn active px-8 bg-[#25252533] text-[#252525] hover:bg-[#FF1F3D] hover:text-white">
                 ${category.category}
               </button>
-        `
-        menuContainer.appendChild(div);
-        
-    });
+        `;
+    menuContainer.appendChild(div);
+  });
+};
 
-}
-
-const loadCardContainer = async(categoryID) =>{
-    const cardContainerId = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryID}`);
-    const data = await cardContainerId.json();
-    console.log(data.status)
-    const cardContainerDetails = data.data;
-    const cardContainer = document.getElementById("card-container");
-    const noDataShow = document.getElementById("no-data-show");
-    cardContainer.innerHTML="";
-    noDataShow.innerHTML='';
-    if(data.status===true){
-    cardContainerDetails.forEach((news)=>{
-        const div = document.createElement("div");
-        const timeInSec = news.others.posted_date;
-        const hours = Math.floor(timeInSec / 3600);
-        const minutes = Math.floor((timeInSec % 3600) / 60);
-       
-        const convertedTime = `${hours}hrs ${minutes}min ago`;
-        
-        div.innerHTML =`
+const loadCardContainer = async (categoryID) => {
+  const cardContainerId = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${categoryID}`
+  );
+  const data = await cardContainerId.json();
+  console.log(data.status);
+  const cardContainerDetails = data.data;
+ 
+  const cardContainer = document.getElementById("card-container");
+  const noDataShow = document.getElementById("no-data-show");
+  cardContainer.innerHTML = "";
+  noDataShow.innerHTML = "";
+  if (data.status === true) {
+    cardContainerDetails.forEach((news) => {
+      // time conversion
+      const div = document.createElement("div");
+      const timeInSec = news.others.posted_date;
+      const hours = Math.floor(timeInSec / 3600);
+      const minutes = Math.floor((timeInSec % 3600) / 60);
+      const convertedTime = `${hours}hrs ${minutes}min ago`;
+      const viewsTotal = news.others.views;
+      div.innerHTML = `
         <div class="card mb-9 ">
         <div class="relative">
           <img src="${news.thumbnail}" alt="" class="w-full h-48 rounded-lg" />
-          ${hours?`<p class="absolute bottom-2 right-2 bg-[#171717] text-white px-2 py-1  rounded text-xs">${convertedTime}</p>
-          `:''}
+          ${
+            hours
+              ? `<p class="absolute bottom-2 right-2 bg-[#171717] text-white px-2 py-1  rounded text-xs">${convertedTime}</p>
+          `
+              : ""
+          }
           </div>
 
         <div class="card-footer flex justify-between mt-8">
@@ -56,22 +61,23 @@ const loadCardContainer = async(categoryID) =>{
             </div>
             <div>
               <h2 class="text-base font-bold">${news.title}</h2>
-              <p id="verified" class="flex align-center  gap-2 my-3 text-[#171717B2] text-sm">${news.authors[0].profile_name} ${news.authors[0].verified ? `<img class="w-5 h-5" src="./images/verified.png" alt="" srcset=""/>` : ''}</p>
+              <p id="verified" class="flex align-center  gap-2 my-3 text-[#171717B2] text-sm">${
+                news.authors[0].profile_name
+              } ${
+        news.authors[0].verified
+          ? `<img class="w-5 h-5" src="./images/verified.png" alt="" srcset=""/>`
+          : ""
+      }</p>
               <p class="text-[#171717B2] text-sm">${news.others.views} views</p>
             </div>
           </div>
         </div>
       </div>
         `;
-        
-      
-        cardContainer.appendChild(div);
-
-        
-        
+      cardContainer.appendChild(div);     
     });
-}else{
-  
+    
+  } else {
     const div = document.createElement("div");
     div.innerHTML = `
     <div class="flex flex-col items-center my-20" >  
@@ -80,12 +86,15 @@ const loadCardContainer = async(categoryID) =>{
     </div>
     `;
     noDataShow.appendChild(div);
+  }
+};
 
-}
-    
-    
-}
+
 
 
 handleCategory();
 loadCardContainer(1000);
+
+
+
+
